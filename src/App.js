5e2d971c50d6cc8730104fd62ac2e66465ea577c
gitirom <wadiaromdane10@gmail.com>
2023-08-21 13:20:06 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+
+
+const App = () => {
+
+
+    return (
+        <>
+            
+            <div  >
+                <h1>Welcome to Type Script course</h1>
+            </div>
+        </>
+        
+    );
+};
+
+export default App;
